e1909a25236a796dd2fd07375822f2d4985ca7a9
LCY <locheukyin520@gmail.com>
2024-07-01 17:24:38 +0800

    add "D501_香城銀行.py"

---
+++ python/D501_香城銀行.py
@@ -0,0 +1,12 @@
+with open('account.txt', 'r') as file:
+    N = int(file.readline().strip())
+    balance = 0
+    for i in range(N):
+        transaction = int(file.readline().strip())
+        balance += transaction
+        
+print(balance)
+    
+    
+
+
