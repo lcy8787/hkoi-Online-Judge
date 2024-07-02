4e19d52dc3558284e06947453a9b7a775a271061
LCY <locheukyin520@gmail.com>
2024-07-02 19:40:40 +0800

    add "D706_循環隊列.py"

---
+++ python/D706_循環隊列.py
@@ -0,0 +1,27 @@
+array = []
+run = int(input())
+def push(num):
+    array.append(num)
+def front():
+    if len(array) != 0:
+        return array[0]
+    else:
+        return "Empty"
+        
+def pop():
+    if len(array) != 0:
+        array.pop(0)
+    else:
+        print("Cannot pop")
+def size():
+    return len(array)
+for i in range(0,run):
+    do = input().split()
+    if do[0] == "PUSH":
+        push(do[1])
+    elif do[0] == "FRONT":
+        print(front())
+    elif do[0] == "POP":
+        pop()
+    elif do[0] == "SIZE":
+        print(size())
